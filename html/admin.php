25735c1d8dca1eef0f2f278da15f9101f7fc517d
GabrielUmann <gabrielumann36@gmail.com>
2023-11-30 02:08:48 -0300

    admin

--- html/admin.php
+++ html/admin.php
@@ -17,7 +17,7 @@ if($_SESSION["user"]["role"] != "ADMIN"){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Admin</title>
         <script src="../assets/admin.js" async></script>
         <link rel="stylesheet" href="../assets/admin.css">
     </head>
